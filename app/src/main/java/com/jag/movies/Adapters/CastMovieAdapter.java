b18c2ae492cc14c46527b0ecdfd8af84d5e3b83a
Joel Acedo <joel.acedo@est.fib.upc.edu>
2017-02-01 16:34:16 +0100

    Added placeholder image to cast view

--- app/src/main/java/com/jag/movies/Adapters/CastMovieAdapter.java
+++ app/src/main/java/com/jag/movies/Adapters/CastMovieAdapter.java
@@ -84,7 +84,7 @@ public class CastMovieAdapter extends RecyclerView.Adapter<CastMovieAdapter.Acto
 
         void renderActorImage(String profileUrl) {
             //TODO Push images
-            imageLoader.bindImage(profileUrl, actorImage);
+            imageLoader.bindImage(profileUrl, actorImage, R.drawable.ic_account_grey600_48dp);
             actorImage.setBorderColor(vibrantColor);
         }
 
