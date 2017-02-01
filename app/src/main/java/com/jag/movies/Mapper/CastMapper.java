537e3bd62d720a3190bdb441adf520937a066d78
Albert Ruiz Lombarte <albert.ruiz.lombarte@est.fib.upc.edu>
2017-02-01 22:12:26 +0100

    Added GetMovieCastInteractor + CastMapper in domain layer

---
+++ app/src/main/java/com/jag/movies/Mapper/CastMapper.java
@@ -0,0 +1,26 @@
+package com.jag.movies.Mapper;
+
+import com.example.entities.Actor;
+import com.jag.movies.UI.Models.ActorViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Albert.Ruiz on 01/02/2017.
+ */
+
+public class CastMapper {
+    public static List<ActorViewModel> toListActorViewModel(List<Actor> actorList) {
+        List<ActorViewModel> actors = new ArrayList<>();
+        for (Actor actor : actorList) {
+            ActorViewModel actorViewModel = toActorViewModel(actor);
+            actors.add(actorViewModel);
+        }
+        return actors;
+    }
+
+    public static ActorViewModel toActorViewModel(Actor actor) {
+        return new ActorViewModel(actor.getName(), actor.getCharacter(), actor.getProfileUrl());
+    }
+}
